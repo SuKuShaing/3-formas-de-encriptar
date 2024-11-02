1e026e9bda4a1729b4e4399931f5ca8e2eab91f7
Sebastian <sebastian.sanhueza.t@usach.cl>
2024-11-02 18:53:44 -0300

    Script para generación de contraseñas encriptadas

---
+++ src/generacion_passwords.py
@@ -0,0 +1,17 @@
+import random
+from werkzeug.security import generate_password_hash
+
+minus = "abcdefghijklmnopqrstuvwxyz"
+mayus = minus.upper()
+numeros = "0123456789"
+simbolos = "@()[]{}*,;/-_¿?.¡!$<#>&+%="
+
+base = minus + mayus + numeros + simbolos
+longitud = 12
+
+for _ in range(10):
+    muestra = random.sample(base, longitud)
+    password = "".join(muestra)
+    password_encriptado = generate_password_hash(password)
+    print("{} => {}".format(password, password_encriptado))
+    print("%s => %s" % (password, password_encriptado))
